b1cf452954fc3f45ae42136b22035bb23a46c351
christian-777 <christian.c.g.f777@hotmail.com>
2023-03-05 15:16:21 -0300

    Add project files.

---
+++ Exercicio_fatorial/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //fatorial
+
+        int num;
+        double fat = 1;
+
+        Console.WriteLine("digite o numero para o fatorial: ");
+        num = int.Parse(Console.ReadLine());
+
+
+        for (int i = num; i > 0; i--)
+        {
+            Console.WriteLine(fat + " * " + i);
+            fat = fat * i;
+        }
+
+        Console.WriteLine("fatorial: " + fat);
+    }
+}
